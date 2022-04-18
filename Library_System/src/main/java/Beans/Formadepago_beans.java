d9660af677294f1024277eca303087b837d37dd9
Kevin2001M <78760245+Kevin2001M@users.noreply.github.com>
2022-04-18 11:54:15 -0600

    SI Forma_de_pago

--- Library_System/src/main/java/Beans/Formadepago_beans.java
+++ Library_System/src/main/java/Beans/Formadepago_beans.java
@@ -4,10 +4,60 @@
  */
 package Beans;
 
+import Entidades.Editorial;
+import Entidades.formaDePago;
+import com.alejandro.BD.ConexionAMYSQL;
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author veget
  */
 public class Formadepago_beans {
-    
+     ConexionAMYSQL con = new ConexionAMYSQL();
+    Connection conexion = con.getConecction();
+
+    //SELECT
+    public ArrayList<formaDePago> ListaFormaDP() {
+        ArrayList<formaDePago> lista = null;
+        try {
+            lista = new ArrayList<formaDePago>();
+
+            CallableStatement cb = conexion.prepareCall("{call SP_S_FORMADEPAGO}");
+            ResultSet resultado = cb.executeQuery();
+
+            while (resultado.next()) {
+                formaDePago ed = new formaDePago();
+                ed.setIdformadepago(resultado.getInt("idForma_De_Pago"));
+                ed.setPago(resultado.getString("Pago"));
+                lista.add(ed);
+            }
+
+        } catch (Exception e) {
+            System.out.println("Error man" + e);
+        }
+
+        return lista;
+    }
+
+    //ADD
+    public void AddFormaDP(formaDePago dp) {
+
+        try {
+            CallableStatement cb = conexion.prepareCall("{call SP_I_FORMADEPAGO(?)}");
+            cb.setString("PPago", dp.getPago());
+            cb.execute();
+
+            JOptionPane.showMessageDialog(null, "Metodo de pago agregado");
+
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "error" + ex);
+        }
+
+    }
 }
--- Library_System/src/main/java/com/alejandro/library_system/FrmFormadePago.java
+++ Library_System/src/main/java/com/alejandro/library_system/FrmFormadePago.java
@@ -4,6 +4,16 @@
  */
 package com.alejandro.library_system;
 
+import Beans.Formadepago_beans;
+import Beans.GeneroLiterario_beans;
+import Entidades.formaDePago;
+import Entidades.generoLiterario;
+import java.awt.event.KeyEvent;
+import java.util.ArrayList;
+import java.util.Iterator;
+import javax.swing.JOptionPane;
+import javax.swing.table.DefaultTableModel;
+
 /**
  *
  * @author Kevin
@@ -16,6 +26,9 @@ public class FrmFormadePago extends javax.swing.JFrame {
     public FrmFormadePago() {
         initComponents();
         this.setLocationRelativeTo(null);
+        carga();
+        Limpiar();
+        txtMetododePago.requestFocus();
     }
 
     /**
@@ -27,11 +40,45 @@ public class FrmFormadePago extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jScrollPane1 = new javax.swing.JScrollPane();
+        jTable1 = new javax.swing.JTable();
+        jScrollPane2 = new javax.swing.JScrollPane();
+        jTable2 = new javax.swing.JTable();
         jLabel1 = new javax.swing.JLabel();
         jLabel2 = new javax.swing.JLabel();
         txtMetododePago = new javax.swing.JTextField();
         btnAgregar = new javax.swing.JButton();
         btnCancelar = new javax.swing.JButton();
+        jLabel3 = new javax.swing.JLabel();
+        txtIdMetodo = new javax.swing.JTextField();
+        jScrollPane3 = new javax.swing.JScrollPane();
+        TblMetodo = new javax.swing.JTable();
+
+        jTable1.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null}
+            },
+            new String [] {
+                "Title 1", "Title 2", "Title 3", "Title 4"
+            }
+        ));
+        jScrollPane1.setViewportView(jTable1);
+
+        jTable2.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null}
+            },
+            new String [] {
+                "Title 1", "Title 2", "Title 3", "Title 4"
+            }
+        ));
+        jScrollPane2.setViewportView(jTable2);
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -48,49 +95,177 @@ public class FrmFormadePago extends javax.swing.JFrame {
         btnAgregar.setFont(new java.awt.Font("Roboto Bk", 1, 14)); // NOI18N
         btnAgregar.setForeground(new java.awt.Color(254, 254, 255));
         btnAgregar.setText("Agregar");
+        btnAgregar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnAgregarActionPerformed(evt);
+            }
+        });
 
         btnCancelar.setBackground(java.awt.Color.blue);
         btnCancelar.setFont(new java.awt.Font("Roboto Bk", 1, 14)); // NOI18N
         btnCancelar.setForeground(new java.awt.Color(254, 254, 255));
         btnCancelar.setText("Cancelar");
+        btnCancelar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnCancelarActionPerformed(evt);
+            }
+        });
+
+        jLabel3.setFont(new java.awt.Font("Roboto Bk", 0, 14)); // NOI18N
+        jLabel3.setText("Id");
+
+        txtIdMetodo.setEditable(false);
+
+        TblMetodo.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null}
+            },
+            new String [] {
+                "Title 1", "Title 2", "Title 3", "Title 4"
+            }
+        ));
+        TblMetodo.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                TblMetodoMouseClicked(evt);
+            }
+        });
+        TblMetodo.addKeyListener(new java.awt.event.KeyAdapter() {
+            public void keyReleased(java.awt.event.KeyEvent evt) {
+                TblMetodoKeyReleased(evt);
+            }
+        });
+        jScrollPane3.setViewportView(TblMetodo);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(72, 72, 72)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                    .addComponent(jLabel2)
-                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                        .addGroup(layout.createSequentialGroup()
-                            .addComponent(btnCancelar)
-                            .addGap(72, 72, 72)
-                            .addComponent(btnAgregar, javax.swing.GroupLayout.PREFERRED_SIZE, 91, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(59, 59, 59)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(btnCancelar)
+                        .addGap(57, 57, 57)
+                        .addComponent(btnAgregar, javax.swing.GroupLayout.PREFERRED_SIZE, 91, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                                .addComponent(jLabel3)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                .addComponent(txtIdMetodo, javax.swing.GroupLayout.PREFERRED_SIZE, 132, javax.swing.GroupLayout.PREFERRED_SIZE))
+                            .addComponent(jLabel2, javax.swing.GroupLayout.Alignment.TRAILING))
                         .addGroup(layout.createSequentialGroup()
                             .addComponent(jLabel1)
-                            .addGap(18, 18, 18)
+                            .addGap(31, 31, 31)
                             .addComponent(txtMetododePago, javax.swing.GroupLayout.PREFERRED_SIZE, 132, javax.swing.GroupLayout.PREFERRED_SIZE))))
-                .addContainerGap(74, Short.MAX_VALUE))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap(13, Short.MAX_VALUE)
+                .addComponent(jScrollPane3, javax.swing.GroupLayout.PREFERRED_SIZE, 371, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addGap(27, 27, 27)
                 .addComponent(jLabel2)
-                .addGap(46, 46, 46)
+                .addGap(57, 57, 57)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(txtIdMetodo, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel3))
+                .addGap(48, 48, 48)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(txtMetododePago, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jLabel1))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 50, Short.MAX_VALUE)
+                .addGap(44, 44, 44)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(btnCancelar, javax.swing.GroupLayout.PREFERRED_SIZE, 33, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(btnAgregar, javax.swing.GroupLayout.PREFERRED_SIZE, 33, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(37, 37, 37))
+                .addGap(34, 34, 34)
+                .addComponent(jScrollPane3, javax.swing.GroupLayout.PREFERRED_SIZE, 208, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    public void carga() {
+        Limpiar();
+        String titulos[] = {"Id", "Metodo de pago"};
+        //Ejemplosdearreglos
+        Double numero[] = new Double[3];
+        DefaultTableModel df = new DefaultTableModel(null, titulos);
+
+        Formadepago_beans es = new Formadepago_beans();
+        ArrayList<formaDePago> listar = es.ListaFormaDP();
+
+        Iterator iterador = listar.iterator();
+        Object fila[] = new Object[5];
+
+        while (iterador.hasNext()) {
+            //CASTEAR
+            formaDePago estBucle = (formaDePago) iterador.next();
+            fila[0] = estBucle.getIdformadepago();
+            fila[1] = estBucle.getPago();
+            df.addRow(fila);
+        }
+        TblMetodo.setModel(df);
+    }
+
+    public void Limpiar() {
+        txtMetododePago.setText("");
+    }
+
+
+    private void btnAgregarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnAgregarActionPerformed
+        if (txtMetododePago.getText().equals("")) {
+            JOptionPane.showMessageDialog(null, "HAY CAMPOS VACIOS");
+            txtMetododePago.requestFocus();
+        } else {
+            formaDePago es = new formaDePago();
+            Formadepago_beans esDAO = new Formadepago_beans();
+
+            es.setPago(txtMetododePago.getText());
+
+            esDAO.AddFormaDP(es);
+            carga();
+            Limpiar();
+
+        }
+    }//GEN-LAST:event_btnAgregarActionPerformed
+
+    private void TblMetodoMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_TblMetodoMouseClicked
+        isSelect = true;
+        try {
+            txtIdMetodo.setText(TblMetodo.getValueAt(TblMetodo.getSelectedRow(), 0).toString());
+            txtMetododePago.setText(TblMetodo.getValueAt(TblMetodo.getSelectedRow(), 1).toString());
+
+        } catch (Exception ex) {
+        }
+    }//GEN-LAST:event_TblMetodoMouseClicked
+
+    private void TblMetodoKeyReleased(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_TblMetodoKeyReleased
+        if ((evt.getKeyCode() == KeyEvent.VK_DOWN) || (evt.getKeyCode() == KeyEvent.VK_UP)) {
+            int filaSeleccionada = this.TblMetodo.getSelectedRow();
+
+            try {
+                this.txtIdMetodo.setText(TblMetodo.getValueAt(filaSeleccionada, 0).toString());
+                this.txtMetododePago.setText(TblMetodo.getValueAt(filaSeleccionada, 1).toString());
+            } catch (Exception e) {
+                JOptionPane.showMessageDialog(null, "Error al leer la tabla", "Error", JOptionPane.WARNING_MESSAGE);
+            }
+
+        }
+    }//GEN-LAST:event_TblMetodoKeyReleased
+
+    private void btnCancelarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnCancelarActionPerformed
+        FrmMenu regresar = new FrmMenu();
+        regresar.setVisible(true);
+        this.setVisible(false);
+    }//GEN-LAST:event_btnCancelarActionPerformed
 
     /**
      * @param args the command line arguments
@@ -127,11 +302,21 @@ public class FrmFormadePago extends javax.swing.JFrame {
         });
     }
 
+    public boolean isSelect = false;
+
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JTable TblMetodo;
     private javax.swing.JButton btnAgregar;
     private javax.swing.JButton btnCancelar;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JScrollPane jScrollPane2;
+    private javax.swing.JScrollPane jScrollPane3;
+    private javax.swing.JTable jTable1;
+    private javax.swing.JTable jTable2;
+    private javax.swing.JTextField txtIdMetodo;
     private javax.swing.JTextField txtMetododePago;
     // End of variables declaration//GEN-END:variables
 }
